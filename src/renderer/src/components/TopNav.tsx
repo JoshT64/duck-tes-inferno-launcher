673c12a70078749faca6e6ec820eda51a5780cbd
JoshT64 <joshuatoney55@gmail.com>
2026-04-12 12:28:49 -0400

    Change tab to home instaed of news

--- src/renderer/src/components/TopNav.tsx
+++ src/renderer/src/components/TopNav.tsx
@@ -8,7 +8,7 @@ interface TopNavProps {
 }
 
 const tabs = [
-  { id: 'home', label: 'Home' },
+  { id: 'news', label: 'News' },
   { id: 'changelog', label: 'Changelog' },
   { id: 'report', label: 'Report Bug' }
 ]
